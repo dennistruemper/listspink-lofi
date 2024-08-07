b8b305d83b1a12ed40e032dfbaf6957352e50584
Dennis Trümper <privat@truemper-nord.de>
2024-08-07 22:02:17 +0200

    update serviceworker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,10 +1,5 @@
-const versionCache = "chache-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/icon512_maskable.png",
-  "/icon512_rounded.png",
-];
+const cacheName = "chache-v1";
+const assets = [];
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
@@ -14,10 +9,27 @@ self.addEventListener("install", (installEvent) => {
   );
 });
 
-self.addEventListener("fetch", (fetchEvent) => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then((res) => {
-      return res || fetch(fetchEvent.request);
+self.addEventListener("fetch", (event) => {
+  // Check if this is a request for an image
+
+  event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      // Go to the cache first
+      return cache.match(event.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
     })
   );
 });
