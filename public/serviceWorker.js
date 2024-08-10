04287fa70daaabea893aa758d978159eb8c4b9de
Dennis Trümper <privat@truemper-nord.de>
2024-08-10 19:45:15 +0200

    add logging

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -31,6 +31,7 @@ self.addEventListener("fetch", (event) => {
 
         // Otherwise, hit the network
         return fetch(event.request).then((fetchedResponse) => {
+          console.log(fetchedResponse.status);
           // Add the network response to the cache for later visits
           cache.put(event.request, fetchedResponse.clone());
 
