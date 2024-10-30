55f2c845d8c4f24c026dcc520603834f67f6c48e
Dennis Trümper <privat@truemper-nord.de>
2024-10-30 18:45:22 +0100

    chore: workaround lamdera to 1.3.1

--- src/RPC.elm
+++ src/RPC.elm
@@ -1,6 +1,12 @@
 module RPC exposing (..)
 
+import Http
+import Json.Encode as Json
+import Lamdera exposing (SessionId)
+import LamderaRPC exposing (..)
+import Types exposing (BackendModel)
 
-dummyFunction : String -> Int
-dummyFunction str =
-    String.length str
+
+lamdera_handleEndpoints : Json.Value -> HttpRequest -> BackendModel -> ( LamderaRPC.RPCResult, BackendModel, Cmd msg )
+lamdera_handleEndpoints reqRaw req model =
+    ( ResultRaw 404 "" [] <| BodyBytes [], model, Cmd.none )
